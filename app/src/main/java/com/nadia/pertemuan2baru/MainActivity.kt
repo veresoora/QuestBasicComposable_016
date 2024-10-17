599f2b5a97843384769d5bed0554814be56f5f67
veresoora <lahanabira@gmail.com>
2024-10-17 23:03:53 +0700

    Mengubah warna dan ukuran Text

--- app/src/main/java/com/nadia/pertemuan2baru/MainActivity.kt
+++ app/src/main/java/com/nadia/pertemuan2baru/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -68,7 +69,9 @@ fun BasicCompose(nadia: Modifier = Modifier) {
         )
 
         Text(
-            text = "Stay tuned on November 6th"
+            text = "Stay tuned on November 6th",
+            color = Color.Red,
+            fontSize = 20.sp
         )
     }
 }
