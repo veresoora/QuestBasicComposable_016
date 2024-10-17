197aecaf3164858203b2b2e2d25ca31c9cbaca1e
veresoora <lahanabira@gmail.com>
2024-10-17 22:22:18 +0700

    Membuat Column yang berada diatas secara vertikal

--- app/src/main/java/com/nadia/pertemuan2baru/MainActivity.kt
+++ app/src/main/java/com/nadia/pertemuan2baru/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -32,7 +34,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BasicCompose(nadia: Modifier = Modifier) {
-
+    Column (
+        verticalArrangement = Arrangement.Top
+    ) {  }
 }
 
 @Composable
