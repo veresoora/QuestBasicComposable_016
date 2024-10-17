8039e85b04ff0553ae10a2d159465e75c0c078c5
veresoora <lahanabira@gmail.com>
2024-10-17 22:47:36 +0700

    Menambahkan gambar pertama dengan 250.dp

--- app/src/main/java/com/nadia/pertemuan2baru/MainActivity.kt
+++ app/src/main/java/com/nadia/pertemuan2baru/MainActivity.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.nadia.pertemuan2baru.ui.theme.Pertemuan2BaruTheme
 
@@ -50,6 +54,12 @@ fun BasicCompose(nadia: Modifier = Modifier) {
 
         Text(
             text = "Welcome to our zoo!!"
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.zootopia),
+            contentDescription = "zootopia",
+            modifier = Modifier.size(250.dp)
         )
     }
 }
