cf45b6ab2f2ee27db51505dc3259038e8ba9ba10
veresoora <lahanabira@gmail.com>
2024-10-17 22:24:20 +0700

    Membuat Column menjadi fullscreen

--- app/src/main/java/com/nadia/pertemuan2baru/MainActivity.kt
+++ app/src/main/java/com/nadia/pertemuan2baru/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.nadia.pertemuan2baru.ui.theme.Pertemuan2BaruTheme
@@ -35,7 +36,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BasicCompose(nadia: Modifier = Modifier) {
     Column (
-        verticalArrangement = Arrangement.Top
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier.fillMaxSize()
     ) {  }
 }
 
