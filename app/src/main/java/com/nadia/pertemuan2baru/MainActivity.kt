6f680a393f3a5bdb72440376d7493d922c2848d7
veresoora <lahanabira@gmail.com>
2024-10-19 01:43:10 +0700

    Mengubah bentuk image

--- app/src/main/java/com/nadia/pertemuan2baru/MainActivity.kt
+++ app/src/main/java/com/nadia/pertemuan2baru/MainActivity.kt
@@ -10,12 +10,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -84,6 +87,8 @@ fun BasicCompose(nadia: Modifier = Modifier) {
             painter = painterResource(id = R.drawable.nadia),
             contentDescription = "Nadia",
             modifier = Modifier.size(350.dp)
+                .clip(RoundedCornerShape(20.dp)),
+            contentScale = ContentScale.Crop
         )
     }
 }
